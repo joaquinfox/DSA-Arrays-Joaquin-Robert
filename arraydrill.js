ac498d724f9aae70a9f052948a30fbb71fb87719
Joaquin Fox <joaquin.fox@gmail.com>
2019-01-16 10:44:52 -0700

    exercises

--- arraydrill.js
+++ arraydrill.js
@@ -67,24 +67,72 @@ class MyArray {
     this.length--;
   }
 }
-MyArray.SIZE_RATIO = 4;
+// MyArray.SIZE_RATIO = 4;
 
-function Main() {
+// function Main() {
+//   let arr = new MyArray();
+//   arr.push(30);
+//   arr.push(1);
+//   arr.push(2);
+//   arr.push(55);
+//   arr.push(9);
+//   arr.push(1, 2, 3, 4, 8);
+//   // console.log(arr.capacity);
+//   // arr.pop(1);
+//   // arr.remove(0);
+//   arr.get(0);
+//   arr.get(1);
+//   arr.get(2);
+//   // arr.insert(1, 69);
+//   console.log(arr);
+// }
+function main() {
+  MyArray.SIZE_RATIO = 3;
+
+  //create an instance of the array class
   let arr = new MyArray();
-  arr.push(30);
-  arr.push(1);
-  arr.push(2);
-  arr.push(55);
-  arr.push(9);
-  arr.push(1, 2, 3, 4, 8);
-  // console.log(arr.capacity);
-  // arr.pop(1);
-  // arr.remove(0);
+
+  //add an item to the array
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+  arr.push(10);
+  arr.pop();
+  arr.pop();
+  arr.pop();
   arr.get(0);
-  arr.get(1);
-  arr.get(2);
-  // arr.insert(1, 69);
+
+  arr.pop();
+  arr.pop();
+  arr.pop();
+  arr.push('tauhida');
   console.log(arr);
+
+  console.log(arr.get(0));
 }
 
-Main();
+main();
+
+/*
+1 What is the length, capacity and memory address of your array?
+length 1, capacity is 3, ptr 0.
+2 What is the length, capacity and memory address of your array? Explain the result of your program after adding the new lines of code.
+The length grows in linear fashion but the capactity grows in (length + 1 )  * 3, once current capactity is exceeded.
+
+3 What is the length, capacity and address of your array? Explain the result of your program after adding the new lines of code
+Length is 3, cpcty 12, address still 3.
+
+4 Print the first item in the array arr.
+Ans 5.
+
+5 Empty the array and add just one item arr.push("tauhida");
+
+6 Print this one item that you just added. What is the result? Can you explain your result?
+
+7 What is the purpose of the _resize() function in your Array class?
+
+
+
+*/
